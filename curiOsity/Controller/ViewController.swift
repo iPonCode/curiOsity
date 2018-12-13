9a26d7d26f32836a5d23a5aa90ad4f305380d13f
superw <sw@swmbp13.local>
2018-12-13 18:38:24 +0100

    Graphical Interface configured and connected with code Changed App colors and configure all text fonts with same family font type and configura the sizes with Autoshrink to match better in small devices. Inserted the App logo with transparency also in the view with an alpha property set and matching by constraints with splash logo that showed previously in launch screen. this is a cool effect, may be more transparency be better. I Created two views for contain others items like labels and button, also I create stack views with similar items to arranges it better. I Used a view also like a ProgressBar, bur is a view. StatusBar: First, I set in the project settings Status Bar Style as "Light" because in launch screen the background was black and I want to see the bar. Then, I set in info.plist file the “Status bar style” value as “UIStatusBarStyleLightContent” and also the “View controller-based status bar appearance” value as “YES” to be able to configure the status bar by code. Finally, I override the preferredStatusBarStyle autocompleted var in the ViewController returning  UIStatusBarStyle.lightContent. Rounded buttons with my extension @IBDesignable extension UIButton, but surely we'll go back to the flat design with square corners. Known issues: I don’t know what that's mean, no warning, but that’s appears in console when run the app: “libMobileGestalt MobileGestalt.c:890: MGIsDeviceOneOfType is not supported on this platform.” Tags: Autoshrink, StatusBar, preferredStatusBarStyle, @IBDesignable, @IBInspectable

--- curiOsity/Controller/ViewController.swift
+++ curiOsity/Controller/ViewController.swift
@@ -11,12 +11,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - DEFINICION DE VARIABLES GLOBALES
+    //aquí conectamos con la intrfaz los elementos que queremos modificar por código en tiempo de ejecución
+    //la etiqueta con la pregunta va a ir cambiando
+    @IBOutlet weak var labelQuestion: UILabel!
+    //la etiqueta con el número de pregunta en la que estamos
+    @IBOutlet weak var labelQuestionNumber: UILabel!
+    //la etiqueta con la puntuación que actualizaremos con cada pregunta respondida
+    @IBOutlet weak var labelScore: UILabel!
+    //iremos modificando el tamaño de la vista con la barra de progreso a medida que avance el juego
+    @IBOutlet weak var viewProgressBar: UIView!
+    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: - BOTONES DE ACCIÓN
+    //ya definimos en el inspector de atributos tag=1 para el botón verdadero y tag=2 para falso y a continuación creamos la acción invocable por cualquier botón UIButton. conectamos los dos botones a este método arrastrando con ctrl pulsado y soltando dentro de la función
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    // MARK: - BARRA DE ESTADO DE LA APLICACIÓN
+    //si definimos en info.plist la clave "View controller-based status bar appearance" con valor "YES" podremos modificar por código el stilo de la barra de estado (Status bar style)
+    //por otro lado, en la pestaña general de los ajustes del proyecto podemos definir Status Bar Style en Light y Hide Status Bar deseleccionado para que durante la carga inicial de la aplicación (launch screen tiene definido un fondo negro) se muestre también la barra de stado con tonos claros, así se verá mejor durante la carga
+    
+    //variable autocomputada con la que podemos sobreescribir el stilo de barra de estado queremos (default o lightContent)
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent //en la aplicación Main Story board tenemos un fondo oscuro y nos interesa mostrar la barra de estado con colores claros para que se vea
+    }
+    
+    //variable autocomputada con la que podemos sobreescribir la preferencia de si se muestra o no la barra de estado devolviendo verdadero para ocultarla o falso para mostrarla
+    /*override var prefersStatusBarHidden: Bool{
+        return true //esto escondería la barra de estado
+    }*/
 }
 
