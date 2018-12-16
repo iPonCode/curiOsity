93267559a27280946b204a88376b87ec57a4a944
superw <sw@swmbp13.local>
2018-12-16 15:47:42 +0100

    Now it's time for translations. And finishing with the App I Translated with localized QuestionsBank.plist to English, French and Spanish with English language like the base Also localized Main.storyboard for this three languages and The hardcoded text inside code (like UIAlert, ProgressHUD and print) was replaced with NSLocalizedString() and a Localizable.strings file I Cleaned code for old comments Tags: NSLocalizedString, Localizable.strings, hardcoded Future possible features: A switch for user can turn on/off QuicklyMode, An All Time HighScore label, Sounds please!, Change logo color when the answer is correct/incorrect and animate the logo in launch screen

--- curiOsity/Controller/ViewController.swift
+++ curiOsity/Controller/ViewController.swift
@@ -51,19 +51,19 @@ class ViewController: UIViewController {
         correctQuestionAnswered = 0
         //por defecto el usuario deberá esperar a que se muestre la alerta para poder responder la siguente pregunta
         QuicklyMode = true //si lo establecemos en true el juego es mas rápido ya que el usuario no tiene que esperar a que desaparezca la alerta (con ProgressHUD) para volver a pulsar el botón
-        //mostramos un mensaje
-        /*
-         ProgressHUD.showSuccess("""
-            Nueva partida!
-            Barajando
-            Mezclado ok
+ 
+        // ALERTA de inicio de partida
+        ProgressHUD.showSuccess("""
+            \(NSLocalizedString("start.game.shuffle.questions", comment: "MessageWelcome"))
             """, interaction: QuicklyMode)
-        */
-        //antes de nada barajamos o mezclamos el array de preguntas para que cada juego se hagan preguntas en distinto orden, no siempre tendrás el mismo orden las preguntas, en cada partida el orden cambiará
+ 
+        //Antes de nada barajamos o mezclamos el array de preguntas para que cada juego se hagan preguntas en distinto orden, no siempre tendrás el mismo orden las preguntas, en cada partida el orden cambiará
         self.factory.questionsBank.questions.shuffle()
-        //genero la primera pregunta
+
+        //Genero la primera pregunta
         askNextQuestion()
-        //llamamos a updateUIElement para poner a cero las etiquetas
+
+        //Llamamos a updateUIElement para poner a cero las etiquetas
         updateUIElements()
     }
     
@@ -80,35 +80,12 @@ class ViewController: UIViewController {
         }
     }
     
-    func gameOver() {
-        //se llama cuando no hay mas preguntas
-        //ALERTA
-        let alerta = UIAlertController(title: "GAME OVER", message: """
-            Has acertado \(self.correctQuestionAnswered) de \(self.currentQuestionID), y has conseguido acumular \(self.currentScore) puntos\n Inténtalo de nuevo!
-            """, preferredStyle: .alert)
-        //let alert = UIAlertController(title: "Fin de partida", message: "Has acertado \(self.correctQuestionAnswered) de \(self.currentQuestionID). Inténtalo de nuevo", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Vale", style: .default) { (_) in
-            self.startGame()
-        }
-        alerta.addAction(okAction) //vinculamos la alerta con la acción
-        present(alerta, animated: true, completion: nil) //mostramos el viewcontroller en pantalla
-        
-    }
-    
     func updateUIElements() {
         //se actualizan las etiquetas y la barra de progreso
-        self.labelScore.text = "Puntucación: \(self.currentScore)"
+        self.labelScore.text = "\(NSLocalizedString("label.score", comment: "LabelScore")): \(self.currentScore)"
         self.labelQuestionNumber.text = "\(self.currentQuestionID)\\\(self.factory.questionsBank.questions.count)"
         
-        /*
-        //pregress bar: establecemos el ancho de la vista de la barra de progreso a lo siguiente:
-        //primero dividimos el ancho total de la pantalla entre el número de preguntas que hay en el banco de preguntas
-        //y luego lo multiplicamos por el numero de pregunta actual de tal forma que cuando la pregunta actual sea igual al número de preguntas total el ancho de la vista será igual al ancho de la pantalla
-        //nota: recuerda que este ancho tiene una restricción muy fuerte en el storyboard que está definido en 30 puntos, por eso no funciona.
-        self.viewProgressBar.frame.size.width = (self.view.frame.size.width/CGFloat(self.factory.questionsBank.questions.count))*CGFloat(self.currentQuestionID)
-         */
-        
-        //Actualizació: modificamos el ancho de la vista barra de estado definiendo un identificador para su constraint width en el story board llamada y la llamamos progressBarWidth, así podemos acceder a esta constraint desde el código
+        //Actualización: modificamos el ancho de la vista barra de estado definiendo un identificador para su constraint width en el story board llamada y la llamamos progressBarWidth, así podemos acceder a esta constraint desde el código
         //recorremos las constraint de la vista hasta dar con la que queremos
         for c in self.viewProgressBar.constraints {
             if c.identifier == "progressBarWidth" {
@@ -121,90 +98,64 @@ class ViewController: UIViewController {
     func analyzeResult (isCorrect : Bool) {
         
         //comprobamos si el usuario ha contestado correctamente
-        //var image : UIImage
         if (isCorrect) { // EL JUGADOR HA ACERTADO
+            
             self.correctQuestionAnswered += 1 //incrementamos el valor de la propiedad que lleva la cuenta del número de preguntas acertadas
             self.currentScore += 100*self.correctQuestionAnswered //si la respuesta es correcta sumamos puntos al Score del usuario, cuantas más preguntas correctas haya acertado más puntos conseguirá
+ 
+            //imprimimos por consola el resultado
             print("""
-            Enhorabuena!
-            Has acertado
-            \(self.currentQuestion.explanation)
-            """)
-            //image.init(named: "logo-curiOsijty-transp-success")
-            //imageViewLogoApp.image = image
-            //mostramos alerta que desaparecerá en unos instantes pero si el usuario pulsa algún botón desaparece inmediatamente
+                \(NSLocalizedString("analyze.result.isCorrect.true", comment: "MessageIsCorrectTrue"))
+                \(self.currentQuestion.explanation)
+                """)
+
+            // ALERTA cuando ACIERTA "analyze.result.isCorrect.true"
+            //mostramos alerta que desaparecerá en unos instantes, si el usuario pulsa algún botón y QucklyMode es true desaparece inmediatamente, si es false tendrá que esperar a que desaparezca para contestar a la siguiente pregunta
             ProgressHUD.showSuccess("""
-            Enhorabuena!
-            Has acertado
-            \(self.currentQuestion.explanation)
-            """, interaction: QuicklyMode)
+                \(NSLocalizedString("analyze.result.isCorrect.true", comment: "MessageIsCorrectTrue"))
+                \(self.currentQuestion.explanation)
+                """, interaction: QuicklyMode)
+
         } else { // EL JUGADOR HA FALLADO
-            //ProgressHUD.showError(self.currentQuestion.explanation)
+
+            //imprimimos por consola el resultado
             print("""
-            Ohoo.. que lástima
-            Has fallado
+            \(NSLocalizedString("analyze.result.isCorrect.false", comment: "MessageIsCorrectFalse"))
             \(self.currentQuestion.explanation)
             """)
-            //actualizamos el titulo de la alerta con el mensaje que corresponde
+            
+            // ALERTA cuando FALLA
             ProgressHUD.showError("""
-            Ohoo.. que lástima
-            Has fallado
-            \(self.currentQuestion.explanation)
-            """, interaction: QuicklyMode)
+                \(NSLocalizedString("analyze.result.isCorrect.false", comment: "MessageIsCorrectFalse"))
+                \(self.currentQuestion.explanation)
+                """, interaction: QuicklyMode)
         }
         
-        //ALERTA (comentamos esta sección porque dejamos de utilizar UIAlert)
-        /*
-        let alert = UIAlertController(title: tituloAlerta, message: self.currentQuestion.explanation, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Vale", style: .default) {(_) in
-            //en el completion handler del botón de la alerta metemos la llamada a la función askNextQuestion siendo necesario hacerlo de forma explícita con la palabra reservada self.
-            //independientemente de si acierta o falla
-            self.askNextQuestion() //generamos la siguiente pregunta
-            self.updateUIElements() //actualizamo la info de las etiquetas
-        }
-        alert.addAction(okAction) //vinculamos la alerta con el botón
-        present(alert, animated: true, completion: nil) //mostramos el viewcontroller por pantalla invocando al metodo present
-        */
         //independientemente de si acierta o falla
         self.askNextQuestion() //generamos la siguiente pregunta
         self.updateUIElements() //actualizamo la info de las etiquetas
     }
 
+    func gameOver() {
+        //se llama cuando no hay mas preguntas, entonces el juego ha terminado
+        //ALERTA de tipo ProgressHUD
+        let alerta = UIAlertController(title: NSLocalizedString("game.over.alert.title", comment: "AlertTitle"), message: "\(NSLocalizedString("game.over.alert.message.1", comment: "Message1")) \(self.correctQuestionAnswered) \(NSLocalizedString("game.over.alert.message.2", comment: "Message2")) \(self.currentQuestionID) \(NSLocalizedString("game.over.alert.message.3", comment: "Message3")). \(NSLocalizedString("game.over.alert.message.4", comment: "Message4")) \(self.currentScore) \(NSLocalizedString("game.over.alert.message.5", comment: "Message5")).\n\(NSLocalizedString("game.over.alert.message.6", comment: "Message6"))", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: NSLocalizedString("game.over.alert.button", comment: "ButtonTitle"), style: .default) { (_) in
+            self.startGame()
+        }
+        
+        alerta.addAction(okAction) //vinculamos la alerta con la acción
+        present(alerta, animated: true, completion: nil) //mostramos el viewcontroller en pantalla
+    }
+    
     // MARK: - BOTONES DE ACCIÓN
     //ya definimos en el inspector de atributos tag=1 para el botón verdadero y tag=2 para falso y a continuación creamos la acción invocable por cualquier botón UIButton. conectamos los dos botones a este método arrastrando con ctrl pulsado y soltando dentro de la función
     @IBAction func buttonPressed(_ sender: UIButton) {
-        /*todo esto ya no es correcto, ya no se hace aquí. ahora cuando se pulsa alguno de los botones se hace otra cosa.
-        // Generamos la batería de preguntas utlizando la factoría
-        let factory = QuestionsFactory()
-        // y le pedimos una pregunta aleatoria cualquiera
-        let question = factory.getRandomQuestion()
-
-        //como ya tenemos sobreescrita la variable description en la clase podemos hacer un print del objeto directamente y también
-        //asignarle a la propiedad text del UILabel que espera un String la versión Stringuizada del objeto directamente
-        print("_________________\n")
-        print(question)
-        
-        
-         //esto va a imprimir el objeto por consola (cuando no tenemos sobreescrita la variable description, tenemos que acceder a las propiedades del objeto para imprimir su valor
-        print("_________________\n")
-        print(question.questionText)
-        print(question.answer)
-        print(question.explanation)
-        
-        // escribimos la pregunta en el text de la etiqueta correspondiente para mostrarla en pantalla asignandole la versión stringuizazada del objeto
-        // labelQuestion.text = "\(question)"
-
-        // también podrímos pasar la versión stringuizada del objeto utilizando el casting a String con el constructor que tiene como parámetro String(describing: Subjet) de la siguiente forma, es decir, pasándole el objeto que tenga sobreescrita la variable computable description
-        //labelQuestion.text = String(describing: question)
-        
-        //aunque todo lo anterior funciona pero ahora me interesa sólo mostrar la pregunta en la etiqueta y no la respuesta ni la explicación así que accederé directamete a la propiedad .text del objeto que es directamente de tipo String, así que no será necesario el casting
-        labelQuestion.text = question.questionText
-        */
-        
         //cuando se pulsa cualquiera de los botones Verdad o Mentira se hace esto
         //comprobamos si la pregunta actual coincide o no con lo que ha contestado el jugador
         var isCorrect : Bool
-        if sender.tag == 1 { //el jugados ha pulsado VERDAD (ya asignamos previamente en los atributos del botón el Verdad el tag = 1)
+        if sender.tag == 1 { //el jugador ha pulsado VERDAD (ya asignamos previamente en los atributos del botón el Verdad el tag = 1)
             isCorrect = (self.currentQuestion.answer == true) //si esta comparación es verdadera (true) entonces que se la respuesta correcta era true y el usuario pulsó Verdad y si es falsa (false) entonces es que la respuesta era false y el usaurio pulsó Mentira
         } else { //el jugador NO ha pulsado VERDAD y asumimos que ha pulsado MENTIRA (ya asignamos previamente en los atributos del botón el Verdad el tag = 2)
             isCorrect = (self.currentQuestion.answer == false) //si esta comparación es verdadera (true) entonces que se la respuesta correcta era false y el usuario pulsó Mentira y si es falsa (false) entonces es que la respuesta era true y el usaurio pulsó Verdad
@@ -217,15 +168,10 @@ class ViewController: UIViewController {
     // MARK: - BARRA DE ESTADO DE LA APLICACIÓN
     //si definimos en info.plist la clave "View controller-based status bar appearance" con valor "YES" podremos modificar por código el stilo de la barra de estado (Status bar style)
     //por otro lado, en la pestaña general de los ajustes del proyecto podemos definir Status Bar Style en Light y Hide Status Bar deseleccionado para que durante la carga inicial de la aplicación (launch screen tiene definido un fondo negro) se muestre también la barra de stado con tonos claros, así se verá mejor durante la carga
-    
     //variable autocomputada con la que podemos sobreescribir el stilo de barra de estado queremos (default o lightContent)
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.lightContent //en la aplicación Main Story board tenemos un fondo oscuro y nos interesa mostrar la barra de estado con colores claros para que se vea
     }
     
-    //variable autocomputada con la que podemos sobreescribir la preferencia de si se muestra o no la barra de estado devolviendo verdadero para ocultarla o falso para mostrarla
-    /*override var prefersStatusBarHidden: Bool{
-        return true //esto escondería la barra de estado
-    }*/
 }
 
--- curiOsity/Model/QuestionsFactory.swift
+++ curiOsity/Model/QuestionsFactory.swift
@@ -6,102 +6,66 @@
 //  Copyright © 2018 superw. All rights reserved.
 //
 
-import Foundation
-
 // FACTORÍA DE PREGUNTAS
 
-// esta clase de la factoría no hereda.
+import Foundation
+
+// Esta clase de la factoría no hereda.
 
 class QuestionsFactory {
     
-    // esta variable será la lista de las preguntas y lo voy a inicializar como un Array de Question vacío (clase definidad en Question.swift)
-    //var questions = [Question]()
-    //estas preguntas que inicialmente eran objetos de un array, ahora que usamos el protocolo Codable y hemos teneido que crear una estructura de modo que podemos sustituilo por una variable de tipo QuestionBank (que es una estructura, ya no es una clase) :
+    //Ahora que usamos el protocolo Codable y hemos teneido que crear una estructura utilizamos una variable de tipo QuestionBank (que es una estructura
     var questionsBank : QuestionsBank! //esta variable la inicializaremos en el constructor Init() de la clase cuando carguemos las preguntas de un fichero plist y la tenemos que hacer requerida ! porque como la inicializamos dentro de un do-catch-error en el caso de que dé error no se incializaría
     
-    // inicializador de la clase para arrancar la batería de preguntas, no tiene parámetros, no necesito parámetros porque será la propia clase que va a saber la lista de contenidos que hay que fabricar
+    //Inicializador de la clase para arrancar la batería de preguntas, no tiene parámetros, no necesito parámetros porque será la propia clase que va a saber la lista de contenidos que hay que fabricar
     init() {
-        /*
-        //inicializamos el array de preguntas apendizandolas al final del mismo con el método append al que pasamos una instancia de tipo Question
-        questions.append(Question(text: "El Vaticano tiene dinero suficiente como para acabar con la pobreza en el mundo", correctAnswer: true, explanationText: "En efecto, disponen de tanto dinero que podrían erradicar la pobreza mundial dos veces!"))
-        questions.append(Question(text: "El caballito de mar es tan fiel a su pareja, que cuando uno muere, el otro también lo hace al poco tiempo", correctAnswer: true, explanationText: "Uno no puede vivir sin el otro"))
-        questions.append(Question(text: "Los humanos son los únicos seres vivos que practican el sexo por placer", correctAnswer: false, explanationText: ""))
-        questions.append(Question(text: "La semilla de la manzana contiene cianuro y es tóxica", correctAnswer: true, explanationText: "Se estima que unas 30-40 semillas podrían matarnos"))
-        */
         
-        // MARK: - PROCESADO MANUAL DEL FICHERO PLIST
-        //ahora queremos dejar de hardcodear metiendo datos en el código que no es una práctica muy correcta y pasar a utilizar un archivo en disco con estos datos así que utilizaremos QuestionBank.plist que contiene un diccionario con todas las preguntas
-        //vamos a procesar este archivo property list y transformarlo en un diccionario
-        //dentro del método Init() tenemos que inicializar un path donde existe el fichero y asegurarnos de que existe
-        //si se puede recuperar el path del fichero indicado entonces leemos el diccionario que resulta del fichero
-        /*if let path = Bundle.main.path(forResource: "QuestionsBank", ofType: "plist") {
-            //NSDictionary crea un diccionario de conveniencia, el término diccionario se refiere a cualquier instancia de una de estas clases sin especificar su membresía de clase exacta
-            if let plist = NSDictionary(contentsOfFile: path){
-                //lo metemos en un if para quitarno sel valor opcional que devuelve así si por lo que sea no lo puede devolver pues no se ejecutará la sentencia let.
-                //ahora que sabemos que hemos podido recuperar ese diccionario con una sola clave llamada questions y que el objeto que resulta de las preguntas es un array
-                //entonces podemos crear una constante
-                let questionData = plist["Questions"] as! [AnyObject] //en este punto de la property list sacamos el primer nivel de objetos que estamos seguros que será un array de objetos
-                print(questionData)
-                
-                //para cada pregunta del array de Questions lo que quiero es recuperar cada pregunta, respuesta y explicación
-                for quest in questionData {
-                    //si podemos recuperar las 3 entonces estamos en disposición de
-                    if let text = quest["question"], let ans = quest["answer"], let expl = quest["explanation"] {
-                        //crear la pregunta forzando un castion al tipo de dato que esperamos que tengan los parámetros ya que si hemos llegado hasta aquí se entiende que el fichero de datos tiene la estructura que esperamos
-                        let q = Question(text: text as! String, correctAnswer: ans as! Bool, explanationText: expl as! String)
-                        //y apendizarla al array, también se podría hacer en un solo paso sin utilizar la constate temporal q:
-                        //questions.append(Question(text: text as! String, correctAnswer: ans as! Bool, explanationText: expl as! String))
-                        questions.append(q) 
-                    }
-                    
-                }
-            }
-        }*/
-        
-        // MARK: - PROCESADO AUTOMÁTICO DEL FICHERO PLIST UTLIZANDO EL PROTOCOLO CODABLE ENCODABLE/DECODABLE
+        // MARK: - PROCESADO AUTOMÁTICO DEL FICHERO PLIST
+        // UTLIZANDO EL PROTOCOLO CODABLE ENCODABLE/DECODABLE
+
         //Ahora vamos a leer los datos del fichero, no el contenido. Es decir, vamos a leer el dato como binario.
-        //en lugar de cargar un path vamos a usar una url y tendremos que protegerlo con la clausula do catch y utlizar try en las llamadas porque pueden generar un error
+        //En lugar de cargar un path vamos a usar una url y tendremos que protegerlo con la clausula do catch y utlizar try en las llamadas porque pueden generar un error
         do {
             if let url = Bundle.main.url(forResource: "QuestionsBank", withExtension: "plist") {
-            //aquí cargamos los datos en binario con un try delante porque puede dar erro indicandole la url
+            //Aquí cargamos los datos en binario con un try delante porque puede dar erro indicandole la url
             let data = try Data(contentsOf: url) //si no tuvieramos esto dentro de un if let deberíamos pasar la url como un parámetro requerido con url! (por el error, podría fallar la extracción de datos binarios de un fichero porque el fichero esté corrupo, o porque esté protegido, etc..)
-            //como es un binario, si imprimos en consola data devolverá el núemro de bytes que ocupa
+            //Como es un binario, si imprimos en consola data devolverá el núemro de bytes que ocupa
             print("Extracción de datos bienarios OK \(data)") //esto imprime 2623 bytes con el fichero plist que se está utlizando en las pruebas
             print("url leída:\n \(url)")
 
-            //aquí tenemos que convertir el fichero data primero en un objeto (una estructura) que es el array QuestionsBank y automáticamente solo con procesarla como QuestionsBank voy a obetener un array de questions (porque estamos utilizando Codable)
-            //vamos directamente a intentar extraer un objeto questionsBank desde data, utlizaremos el PropertyListDecoder
-            //como la descodificación puede dar error lo protegemos con un try
+            //Aquí tenemos que convertir el fichero data primero en un objeto (una estructura) que es el array QuestionsBank y automáticamente solo con procesarla como QuestionsBank voy a obetener un array de questions (porque estamos utilizando Codable)
+            //Vamos directamente a intentar extraer un objeto questionsBank desde data, utlizaremos el PropertyListDecoder
+            //Como la descodificación puede dar error lo protegemos con un try
             self.questionsBank = try PropertyListDecoder().decode(QuestionsBank.self, from: data)
-            //si esto ha funcionado entonces tendremos un campo llamado questions que son las preguntas que van a ir en la factoría
-            //self.questions = questionBank.questions //cargamos las preguntas
-            //Actualización: me cargo la línea anterior porque ahora trabajamos con la variable de tipo estructura QuestionsBank y no con la variable questions que era un array de objetos de tipo Question
+            //Si esto ha funcionado entonces tendremos un campo llamado questions que son las preguntas que van a ir en la factoría
             }
         } catch {
             print(error)
         }
         
-        if self.questionsBank == nil { //si entra aquí la apilcación peta (por ejemplo, no encuentra o no existe archivo QuestionsBank.plist
+        //Si entra aquí la apilcación peta (por ejemplo, no encuentra o no existe archivo QuestionsBank.plist
+        if self.questionsBank == nil {
             //justo antes del error en tiempo de ejecución imprimiríamos este mensaje por consola
             print(">>> ERROR FATAL! <<< \n>>> NO SE HA PODIDO INICIALIZAR la variable questionBank de tipo estructura QuestionBank <<<")
         }
 
     }
     
-    // esta función devolverá la pregunta que se encuentre en el índice que recibe y el índice está fuera de rango devuelve nil, por eso el parámetro de retorno Question? es opcional
+    //Esta función devolverá la pregunta que se encuentre en el índice que recibe y si el índice está fuera de rango devuelve nil, por eso el parámetro de retorno Question? es opcional
     func getQuestionAt (index : Int) -> Question? {
         print(">>> VALOR DE index es \(index) y el valor de COUNT es \(self.questionsBank.questions.count)")
         if index<0 || index>self.questionsBank.questions.count - 1 { //comprobamos que el índice recibido no esté fuera de rango y si lo está devolvemos nil
             return nil
         } else {
-            // si el índice está dentro del rago de elementos del array devolvermos el valor del elemento en esa posición que será un objeto de tipo Question
+            //Si el índice está dentro del rago de elementos del array devolvermos el valor del elemento en esa posición que será un objeto de tipo Question
             return self.questionsBank.questions[index]
         }
     }
     
-    // esta función devolverá una pregunta aleatoria y no recibe ningún parámetro
+    //Esta función devolverá una pregunta aleatoria y no recibe ningún parámetro
+    //Actualización: Se deja como referencia, se utilizó durante el desarrollo
     func getRandomQuestion () -> Question {
-        // creamos una constante local al que asignamos un valor aleatorio de tipo entero utilizando la función arc4random_.. y haciendo los casting necesarios ya que requiere de un tipo entero sin signo de 32bit (UInt32) y nuestro índice debe ser entero (Int)
+        //Creamos una constante local al que asignamos un valor aleatorio de tipo entero utilizando la función arc4random_.. y haciendo los casting necesarios ya que requiere de un tipo entero sin signo de 32bit (UInt32) y nuestro índice debe ser entero (Int)
         let index = Int(arc4random_uniform(UInt32(self.questionsBank.questions.count)))
         return self.questionsBank.questions[index]
     }
